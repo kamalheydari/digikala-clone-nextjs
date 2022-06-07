8bc010b0155820a703ad42b4a3b4a32a6638543e
kamal heydari <kamal3heydari@gmail.com>
2022-06-07 13:52:35 +0430

    feat vazir fonts

---
+++ pages/_document.js
@@ -0,0 +1,36 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='fa'>
+        <Head>
+          <link
+            rel='preload'
+            href='/fonts/vazir/Vazirmatn-Regular.woff2'
+            as='font'
+            crossOrigin='anonymous'
+          ></link>
+          <link
+            rel='preload'
+            href='/fonts/vazir/Vazirmatn-Medium.woff2'
+            as='font'
+            crossOrigin='anonymous'
+          ></link>
+          <link
+            rel='preload'
+            href='/fonts/vazir/Vazirmatn-Bold.woff2'
+            as='font'
+            crossOrigin='anonymous'
+          ></link>
+        </Head>
+        <body>
+          <Main></Main>
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        vazir: "Vazir",
+      },
+    },
   },
   plugins: [],
 };
