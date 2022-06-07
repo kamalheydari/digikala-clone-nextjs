51020b72ac5c39f342a5bfa087510bc1c2c98512
kamal heydari <kamal3heydari@gmail.com>
2022-06-07 12:50:35 +0430

    feat auth slice

--- app/store.js
+++ app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import fetchApi from "app/slices/fetchApiSlice";
+import authReducer from "./slices/authSlice";
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     [fetchApi.reducerPath]: fetchApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
