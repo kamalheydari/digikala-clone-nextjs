9ab57244387c3809b8fcbb56ad2b4caa309f833e
kamal heydari <kamal3heydari@gmail.com>
2022-06-07 13:24:02 +0430

    feat cart slice

--- app/store.js
+++ app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import fetchApi from "app/slices/fetchApiSlice";
 import authReducer from "./slices/authSlice";
+import cartReducer from "./slices/cartSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    cart: cartReducer,
     [fetchApi.reducerPath]: fetchApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
