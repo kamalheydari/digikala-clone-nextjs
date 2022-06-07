2ab37593483cca2fa271197dbb83840a1158c665
kamal heydari <kamal3heydari@gmail.com>
2022-06-07 15:18:06 +0430

    feat rtl direction

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang='fa'>
+      <Html  lang="fa" dir="rtl">
         <Head>
           <link
             rel='preload'
