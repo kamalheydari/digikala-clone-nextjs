3c5d653e0801f40db83ef7ed74af03726b683637
kamal heydari <kamal3heydari@gmail.com>
2022-06-08 17:11:02 +0430

    add toast config

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
 import "/styles/globals.css";
 
+import { Toaster } from "react-hot-toast";
+
 //? store
 import { store } from "app/store";
 import { Provider } from "react-redux";
-
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
+      <Toaster
+        position='top-right'
+        toastOptions={{ style: { fontSize: "1rem" } }}
+      />
     </Provider>
   );
 }
