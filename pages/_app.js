d403d0ee601744635f08c80c52598ff064accfe7
kamal heydari <kamal3heydari@gmail.com>
2022-06-10 13:55:20 +0430

    Fix Hydration failed

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,20 @@ import { Layout } from "components";
 //? store
 import { store } from "app/store";
 import { Provider } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function MyApp({ Component, pageProps }) {
+
+//? Fix Hydration failed
+  const [showChild, setShowChild] = useState(false);
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+  
   //? Lyout Config
   if (Component.getLayout) {
     return Component.getLayout(
