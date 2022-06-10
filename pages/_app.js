ae26a8eb6baec6f27da91e0e6345c150c71e9efc
kamal heydari <kamal3heydari@gmail.com>
2022-06-10 16:39:14 +0430

    config layouts

--- pages/_app.js
+++ pages/_app.js
@@ -6,10 +6,10 @@ import { Layout } from "components";
 import { store } from "app/store";
 import { Provider } from "react-redux";
 import { useEffect, useState } from "react";
+import DashboardLayout from "components/DashboardLayout";
 
 export default function MyApp({ Component, pageProps }) {
-
-//? Fix Hydration failed
+  //? Fix Hydration failed
   const [showChild, setShowChild] = useState(false);
   useEffect(() => {
     setShowChild(true);
@@ -18,19 +18,32 @@ export default function MyApp({ Component, pageProps }) {
   if (!showChild) {
     return null;
   }
-  
+
   //? Lyout Config
-  if (Component.getLayout) {
-    return Component.getLayout(
+  if (Component.getClientLayout) {
+    return Component.getClientLayout(
       <Provider store={store}>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    );
+  }
+
+  //? Lyout Config
+  if (Component.getDashboardLayout) {
+    return Component.getDashboardLayout(
+      <Provider store={store}>
+        <DashboardLayout>
+          <Component {...pageProps} />
+        </DashboardLayout>
       </Provider>
     );
   }
 
   return (
     <Provider store={store}>
-      <Layout></Layout>
+      <Component {...pageProps} />
     </Provider>
   );
 }
