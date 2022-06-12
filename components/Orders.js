2acc86ef85c52d008c7a367550f0fb13893c68a2
kamal heydari <kamal3heydari@gmail.com>
2022-06-12 12:44:22 +0430

    feat reusable components

---
+++ components/Orders.js
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import { ArrowLink } from "components";
+
+export default function Orders() {
+  return (
+    <>
+      <div className=' py-6 lg:py-0'>
+        <div className='flex justify-between mb-7 px-5'>
+          <h2 className='inline-block py-1 border-b-2 border-red-500'>
+            سفارش‌های من
+          </h2>
+          <ArrowLink path='profile/orders'>مشاهده همه</ArrowLink>
+        </div>
+        <div className='flex justify-evenly lg:py-20'>
+          <div className='flex flex-col items-center lg:flex-row lg:gap-x-2'>
+            <div className='relative w-14 h-14 '>
+              <Image src='/images/status-processing.svg' layout='fill' />
+              <span className='absolute order-badge'>0</span>
+            </div>
+            <div className='text-gray-700'>
+              <span className='hidden lg:block'> 0 سفارش</span>
+              <span>جاری</span>
+            </div>
+          </div>
+
+          <div className="section-divide-x"/>
+
+
+          <div className='flex flex-col items-center lg:flex-row lg:gap-x-2'>
+            <div className='relative w-14 h-14 '>
+              <Image src='/images/status-delivered.svg' layout='fill' />
+              <span className='absolute order-badge'>0</span>
+            </div>
+            <div className='text-gray-700'>
+              <span className='hidden lg:block'> 0 سفارش</span>
+              <span>تحویل شده</span>
+            </div>
+          </div>
+
+          <div className="section-divide-x"/>
+
+          <div className='flex flex-col items-center lg:flex-row lg:gap-x-2'>
+            <div className='relative w-14 h-14 '>
+              <Image src='/images/status-returned.svg' layout='fill' />
+              <span className='absolute order-badge'>0</span>
+            </div>
+            <div className='text-gray-700'>
+              <span className='hidden lg:block'> 0 سفارش</span>
+              <span>مرجوع</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='section-divide-y' />
+    </>
+  );
+}
---
+++ components/ProfileAside.js
@@ -0,0 +1,73 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Icons, BoxLink, Logout,Orders } from "components";
+
+export default function ProfileAside({user}) {
+  const profilePaths = [
+    {
+      name: "سفارش‌ها",
+      icon: <Icons.Bag className='icon text-black' />,
+      path: "/profile/orders",
+    },
+    {
+      name: "لیست‌های من",
+      icon: <Icons.Heart className='icon text-black' />,
+      path: "/profile/lists",
+    },
+    {
+      name: "دیدگاه‌ها",
+      icon: <Icons.Comment className='icon text-black' />,
+      path: "/profile/comments",
+    },
+    {
+      name: "آدرس‌ها",
+      icon: <Icons.Location className='icon text-black' />,
+      path: "/profile/addresses",
+    },
+    {
+      name: "بازدید‌های اخیر",
+      icon: <Icons.Clock className='icon text-black' />,
+      path: "/profile/user-history",
+    },
+    {
+      name: "اطلاعات حساب کاربری",
+      icon: <Icons.User className='icon text-black' />,
+      path: "/profile/personal-info",
+    },
+  ];
+
+  return (
+    <aside className='mt-6 lg:border lg:border-gray-200 lg:rounded-md lg:py-4'>
+      <div className='px-5 py-2 flex justify-between items-center '>
+        <div className='relative w-12 h-12'>
+          <Image src={user.avatar} layout='fill' />
+        </div>
+        <span className='ml-auto mr-2 font-bold '>{user.name}</span>
+        <Link href='profile/personal-info'>
+          <a>
+            <Icons.Edit className='text-blue-400 w-6 h-6' />
+          </a>
+        </Link>
+      </div>
+
+      <div className='lg:hidden'>
+        <Orders />
+      </div>
+
+      <div className='mt-7'>
+        <div className='hidden lg:block'>
+          <BoxLink name='خلاصه فعالیت ‌ها' path='/profile'>
+            <Icons.Home className='icon text-black' />
+          </BoxLink>
+        </div>
+        {profilePaths.map((item, index) => (
+          <BoxLink key={index} path={item.path} name={item.name}>
+            {item.icon}
+          </BoxLink>
+        ))}
+        <Logout />
+      </div>
+    </aside>
+  );
+}
