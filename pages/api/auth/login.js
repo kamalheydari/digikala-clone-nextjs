42137eaa4470d479b95ed2fbf6d9273d1706560a
kamal heydari <kamal3heydari@gmail.com>
2022-06-13 21:05:27 +0430

    fix typo

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -31,7 +31,7 @@ export default async (req, res) => {
               name: user.name,
               email: user.email,
               mobile: user.mobile,
-              address: newUser.address,
+              address: user.address,
               role: user.role,
               root: user.root,
             },
