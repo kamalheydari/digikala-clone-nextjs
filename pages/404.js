592b3eb17ba601f40854a0656f9b51e9c879b780
kamal heydari <kamal3heydari@gmail.com>
2022-08-06 23:36:28 +0430

    feat 404

---
+++ pages/404.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+import { ArrowLink } from "components";
+
+export default function NotFoundPage() {
+  return (
+    <main className="flex items-center justify-center flex-col gap-y-6 py-8 xl:mt-28">
+      <p className="text-black text-base font-semibold">صفحه‌ای که دنبال آن بودید پیدا نشد!</p>
+      <ArrowLink path='/'>صفحه اصلی</ArrowLink>
+      <div className='relative w-full h-72 max-w-lg'>
+        <Image src='/icons/page-not-found.png' layout='fill' />
+      </div>
+    </main>
+  );
+}
+
+NotFoundPage.getClientLayout = function pageLayout(page) {
+    return <>{page}</>;
+  };
+  
