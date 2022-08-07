d114aa042ed5362ce8e6c496a258f118c99e6e72
kamal heydari <kamal3heydari@gmail.com>
2022-08-08 02:09:38 +0430

    fix environment variable

--- next.config.js
+++ next.config.js
@@ -5,9 +5,9 @@ module.exports = {
     MONGODB_URL: "mongodb+srv://digikala-clone-nextjs:digikala-clone-nextjs123456@cluster0.k5i8lxq.mongodb.net/digikala-clone-nextjs?retryWrites=true&w=majority",
     NEXT_PUBLIC_ACCESS_TOKEN_SECRET: "h1n0U6LHJtCZuWitwjn3oLd5qCRIgUFtemnjTrpfZLzVZ3ff0f",
     ACCESS_TOKEN_SECRET: "h1n0U6LHJtCZuWitwjn3oLd5qCRIgUFtemnjTrpfZLzVZ3ff0f",
-    CLOUD_UPDATE_PRESET:"nextjs_store",
-    CLOUD_NAME:"djvgq77b0",
-    CLOUD_API:"https://api.cloudinary.com/v1_1/djvgq77b0/image/upload",
+    NEXT_PUBLIC_CLOUD_UPDATE_PRESET:"nextjs_store",
+    NEXT_PUBLIC_CLOUD_NAME:"djvgq77b0",
+    NEXT_PUBLIC_CLOUD_API:"https://api.cloudinary.com/v1_1/djvgq77b0/image/upload",
   },
   images: {
     domains: ["res.cloudinary.com"],
--- utils/imageUpload.js
+++ utils/imageUpload.js
@@ -3,10 +3,10 @@ export const imageUpload = async (images) => {
   for (const item of images) {
     const formData = new FormData();
     formData.append("file", item);
-    formData.append("upload_preset", process.env.CLOUD_UPDATE_PRESET);
-    formData.append("cloud_name", process.env.CLOUD_NAME);
+    formData.append("upload_preset", process.env.NEXT_PUBLIC_CLOUD_UPDATE_PRESET);
+    formData.append("cloud_name", process.env.NEXT_PUBLIC_CLOUD_NAME);
 
-    const res = await fetch(process.env.CLOUD_API, {
+    const res = await fetch(process.env.NEXT_PUBLIC_CLOUD_API, {
       method: "POST",
       body: formData,
     });
