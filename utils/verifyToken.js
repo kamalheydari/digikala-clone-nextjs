aaa666299fefccdd8129f763a15472d1da411861
kamal heydari <kamal3heydari@gmail.com>
2022-08-08 01:57:09 +0430

    fix environment variable

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -2,7 +2,7 @@ import jwt, { decode } from "jsonwebtoken";
 
 export default function verifyToken(token) {
   let status = true;
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+  jwt.verify(token, process.env.NEXT_PUBLIC_ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) status = false;
   });
   return status;
