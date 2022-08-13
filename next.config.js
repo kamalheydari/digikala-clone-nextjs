637d2cb05e7a42c96a82d945d3924bdeb847d919
kamal heydari <kamal3heydari@gmail.com>
2022-08-13 20:50:07 +0430

    dev env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   env: {
-    NEXT_PUBLIC_BASE_URL: "https://digikala-clone-heydari.iran.liara.run",
-    MONGODB_URL:
-      "mongodb://root:3F6NVtZZctS0lfIzeWUJsjuT@finn.iran.liara.ir:34423/digikala-clone-nexjs?authSource=admin",
-    // NEXT_PUBLIC_BASE_URL: "http://localhost:3000",
-    // MONGODB_URL: "mongodb://localhost:27017/digikala-clone-nextjs",
+    NEXT_PUBLIC_BASE_URL: "http://localhost:3000",
+    MONGODB_URL: "mongodb://localhost:27017/digikala-clone-nextjs",
     NEXT_PUBLIC_ACCESS_TOKEN_SECRET:
       "h1n0U6LHJtCZuWitwjn3oLd5qCRIgUFtemnjTrpfZLzVZ3ff0f",
     ACCESS_TOKEN_SECRET: "h1n0U6LHJtCZuWitwjn3oLd5qCRIgUFtemnjTrpfZLzVZ3ff0f",
@@ -14,6 +11,9 @@ module.exports = {
       "https://api.cloudinary.com/v1_1/djvgq77b0/image/upload",
   },
   images: {
-    domains: ["res.cloudinary.com","digikala-clone-heydari-db.storage.iran.liara.space"],
-  }
+    domains: [
+      "res.cloudinary.com",
+      "digikala-clone-heydari-db.storage.iran.liara.space",
+    ],
+  },
 };
