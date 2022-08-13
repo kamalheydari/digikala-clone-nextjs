d015698ff4c7bc885209f8d0c174cfbfbd5ef9cc
kamal heydari <kamal3heydari@gmail.com>
2022-08-13 13:20:37 +0430

    fix auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,20 +2,19 @@ import sendError from "utils/sendError";
 import jwt from "jsonwebtoken";
 import Users from "models/User";
 import db from "lib/db";
-import mongoose from "mongoose";
 
 export default async function auth(req, res) {
   const token = req.headers.authorization;
 
   if (!token) sendError(res, 400, "توکن احراز هویت نامعتبر است");
 
-  const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  const decoded = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
   if (!decoded) sendError(res, 400, "توکن احراز هویت نامعتبر است");
-
-  db.connect();
+console.log(decoded)
+  await db.connect();
   const user = await Users.findOne({ _id: decoded.id });
-  db.disconnect();
+  await db.disconnect();
 
   return { id: decoded.id, role: user.role, root: user.root };
 }
