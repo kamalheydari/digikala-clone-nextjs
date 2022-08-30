f8938db9d7e92e6a60a0b46b58e05c678c2754ee
kamal heydari <kamal3heydari@gmail.com>
2022-08-30 17:08:55 +0430

    feat handle update

---
+++ components/share/HandleUpdate.js
@@ -0,0 +1,59 @@
+import { useEffect } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import { showAlert } from "app/slices/alert.slice";
+import { confirmReset } from "app/slices/modal.slice";
+
+export default function HandleUpdate({
+  updateFunc,
+  isSuccess,
+  isError,
+  error,
+  data,
+}) {
+  const dispatch = useDispatch();
+
+  //? Store
+  const { isConfirmUpdate, id, editedData } = useSelector(
+    (state) => state.modal
+  );
+  const { token } = useSelector((state) => state.user);
+
+  //? Send Request
+  useEffect(() => {
+    if (isConfirmUpdate) {
+      updateFunc({
+        id,
+        token,
+        body: { ...editedData },
+      });
+    }
+  }, [isConfirmUpdate]);
+
+  //? Handle Response
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(
+        showAlert({
+          status: "success",
+          title: data.msg,
+        })
+      );
+      dispatch(confirmReset());
+    }
+  }, [isSuccess]);
+
+  useEffect(() => {
+    if (isError) {
+      dispatch(
+        showAlert({
+          status: "error",
+          title: error.data.err,
+        })
+      );
+      dispatch(confirmReset());
+    }
+  }, [isError]);
+
+  return null;
+}
