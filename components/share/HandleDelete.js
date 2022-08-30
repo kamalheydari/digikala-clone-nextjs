4711c1f21e98e0e16dcb5af293299b93a6be8f0e
kamal heydari <kamal3heydari@gmail.com>
2022-08-30 17:08:36 +0430

    feat handle delete

---
+++ components/share/HandleDelete.js
@@ -0,0 +1,57 @@
+import { useEffect } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import { showAlert } from "app/slices/alert.slice";
+import { resetDetails } from "app/slices/details.slice";
+import { confirmReset } from "app/slices/modal.slice";
+
+export default function HandleDelete({
+  deleteFunc,
+  isSuccess,
+  isError,
+  error,
+  data,
+}) {
+  const dispatch = useDispatch();
+  //? Store
+  const { isConfirmDelete, id, type } = useSelector((state) => state.modal);
+  const { token } = useSelector((state) => state.user);
+
+  //? Send Request
+  useEffect(() => {
+    if (isConfirmDelete) {
+      deleteFunc({
+        id,
+        token,
+      });
+    }
+  }, [isConfirmDelete]);
+
+  //? Handle Response
+  useEffect(() => {
+    if (isSuccess) {
+      if (type === "confirm-delete-details") dispatch(resetDetails());
+      dispatch(
+        showAlert({
+          status: "success",
+          title: data.msg,
+        })
+      );
+      dispatch(confirmReset());
+    }
+  }, [isSuccess]);
+
+  useEffect(() => {
+    if (isError) {
+      dispatch(
+        showAlert({
+          status: "error",
+          title: error?.data.err,
+        })
+      );
+      dispatch(confirmReset());
+    }
+  }, [isError]);
+
+  return null;
+}
