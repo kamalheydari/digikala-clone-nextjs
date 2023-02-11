99cf519b220a9e6b766a870ff405c03fb774a80e
kamal heydari <kamal3heydari@gmail.com>
2023-02-11 23:25:50 +0330

    fix display errors

--- components/common/DisplayError.js
+++ components/common/DisplayError.js
@@ -1,10 +1,23 @@
+import { Transition } from "@headlessui/react";
 import Icons from "./Icons";
 
 export default function DisplayError({ errors }) {
   return (
-      <div className='error-msg'>
-        <Icons.Exclamation />
-        <span>{errors?.message}</span>
-      </div>
+    <div className='min-h-[29px]'>
+      <Transition
+        show={!!errors}
+        enter='transition-opacity duration-150'
+        enterFrom='opacity-0'
+        enterTo='opacity-100'
+        leave='transition-opacity duration-10'
+        leaveFrom='opacity-100'
+        leaveTo='opacity-0'
+      >
+        <div className='display-error'>
+          {errors && <Icons.Exclamation />}
+          <span>{errors?.message}</span>
+        </div>
+      </Transition>
+    </div>
   );
 }
