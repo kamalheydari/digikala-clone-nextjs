1cd75cd1f4ea89d92d4256fb6b6234dea449d760
kamal heydari <kamal3heydari@gmail.com>
2023-02-19 15:54:29 +0330

    add @svgr/webpack

--- next.config.js
+++ next.config.js
@@ -25,4 +25,11 @@ module.exports = {
   experimental: {
     outputStandalone: true,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: "@svgr/webpack",
+    });
+    return config;
+  },
 };
