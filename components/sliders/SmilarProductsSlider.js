2a0007aac313e296637cc9c5988575ad8edd485c
kamal heydari <kamal3heydari@gmail.com>
2023-03-17 16:19:05 +0330

    fix title style

--- components/sliders/SmilarProductsSlider.js
+++ components/sliders/SmilarProductsSlider.js
@@ -13,7 +13,7 @@ import { ProductCard } from 'components'
 export default function SmilarProductsSlider({ smilarProducts }) {
   return (
     <section className='px-3 py-4 overflow-hidden lg:border lg:border-gray-100 lg:rounded-md'>
-      <h4 className='mb-3 lg:border-b-2 lg:border-red-500 w-fit'>
+      <h4 className='mb-3 lg:border-b-2 lg:border-red-500'>
         {smilarProducts.title}
       </h4>
       <Swiper
