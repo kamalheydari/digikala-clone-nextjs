0ecafbfda38f8636cfcdfc9eaf3547456119c5c1
kamal heydari <kamal3heydari@gmail.com>
2024-03-02 12:13:48 +0330

    fix imports

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,13 +1,14 @@
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
 
-import { ClientLayout } from '@/components/layouts'
+import { Header, Footer } from '@/components/shared'
 import { ArrowLink, ResponsiveImage } from '@/components/ui'
 
 function NotFoundPage() {
   // ? Render(s)
   return (
-    <ClientLayout>
+    <>
+      <Header />
       <main className="flex flex-col items-center justify-center gap-y-6 py-8 xl:mt-28">
         <Head>
           <title>دیجی‌کالا | 404</title>
@@ -16,7 +17,8 @@ function NotFoundPage() {
         <ArrowLink path="/">صفحه اصلی</ArrowLink>
         <ResponsiveImage dimensions="w-full max-w-lg h-72" src="/icons/page-not-found.png" alt="404" />
       </main>
-    </ClientLayout>
+      <Footer />
+    </>
   )
 }
 
