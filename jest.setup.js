9c73d9debd277f0a0d3ada73577bc203c622c268
unknown <kamal3heydari@gmail.com>
2024-03-20 20:56:26 +0330

    fix jest config

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,4 @@
 import '@testing-library/jest-dom'
 
-global.TextEncoder = require('util').TextEncoder
-global.TextDecoder = require('util').TextDecoder
+// global.TextEncoder = require('util').TextEncoder
+// global.TextDecoder = require('util').TextDecoder
